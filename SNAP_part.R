abfd1a8af30ab73e095cc6c261bf3c6057a81138
dmurray@ui.urban.org <dmurray@ui.urban.org>
2019-04-22 11:55:29 -0400

    tweaks on the graphic

--- SNAP_part.R
+++ SNAP_part.R
@@ -18,22 +18,32 @@ library(reticulate)
 library(gapminder)
 library(sys)
 
+# set up for urban style
 set_urbn_defaults(style = "print")
+
+# make sure we are in the right working directory
 setwd("D:\\python\\SafetyNet\\DKAN")
 
-
+# call python programs - note that we make sure we know which python we're using
 use_python("D:\\Users\\dmurray\\AppData\\Local\\Programs\\Python\\Python36")
 source_python("D:/python/SafetyNet/DKAN/safetynet_dkan.py")
 source_python("D:/python/SafetyNet/DKAN/test_SNA.py")
 
-mytitle    =  df_meta["statistics_label"]
 
-ggplot(data = df_data, mapping = aes(x = year, y = as.integer(data), group=1)) +
+# following the call, we should have the two dataframes from thy python functions
+mytitle    =  df_meta["statistics_label"]
+mynotes    =  df_meta["notes"]
+
+# pass the data and labels to ggplot
+q <- ggplot(data = df_data, mapping = aes(x = year, y = as.integer(data), group=1)) +
   geom_line() +
   scale_y_continuous(labels = scales::comma) +
+  scale_x_discrete() +
   labs(title = mytitle,
-       caption = "Urban Institute",
+       caption = mynotes,
        x = "Year",
        y = "Thousands of Households")
 
+q + theme(axis.text.x = element_text(angle = 90, hjust = 1))
 
+# that's all folks...
--- test_SNA.py
+++ test_SNA.py
@@ -3,11 +3,15 @@
 import safetynet_dkan as sd
 
 mystatid = 101
+print ("\nCalling validate_statid...\n")
 myprogram_name = sd.validate_statid(mystatid)
 print("Program name is",myprogram_name," for statid ",mystatid)
 
+print ("\nCalling get_SNA_data...\n")
 df_data = sd.get_SNA_data(mystatid)
+print ("\nCalling get_SNA_meta...\n")
 df_meta = sd.get_SNA_meta(mystatid)
+print("\nDone")
 
 
 
